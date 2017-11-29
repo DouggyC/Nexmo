6c24c9c63bb063d7e95b731c83c293e4bf822a2d
DouggyC <dhcan8@gmail.com>
2017-11-28 18:41:28 -0800

    Learning_NexmoAPI_Node-app

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// Init Nexmo
+const nexmo = new Nexmo({
+  apiKey: '9ac462d8',
+  apiSecret: '7454982f3784591d'
+}, {debug: true});
+
+// Init App
+const app = express();
+
+//Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Catch form submit
+app.post('/', (req, res) => {
+  // res.send(req.body);
+  // console.log(req.body);
+  const number = req.body.number;
+  const text = req.body.text;
+
+  nexmo.message.sendSms(
+    '12035638565', number, text, {type: 'unicode'},
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+      }
+    }
+  );
+});
+
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
